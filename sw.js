a806764d899c3cecca15bf9ed22130059290e3e2
lily0528 <43476261+lily0528@users.noreply.github.com>
2018-12-19 20:07:27 -0600

    Add files via upload
    Add Sevice Worker

---
+++ sw.js
@@ -0,0 +1,41 @@
+const staticCache = 'my-cache-1';
+self.addEventListener('activate', event =>{
+    event.waitUntil(
+        caches.keys().then(function(cacheNames){
+            return Promise.all(
+            cacheNames.filter(function(cacheNames){
+                return cacheName.startsWith('my-')&& cacheName !== staticCache
+            }).map(function(cacheName){
+                return caches.delete(cacheName);
+            })
+            );
+        })
+    );
+})
+self.addEventListener('install',(event) =>{
+    event.waitUntil(
+        caches.open(staticCache).then(function(cache){
+        return cache.addAll(
+            ['/images/line.jpg',
+            '/images/logo.jpg',
+            '/images/main2.jpg',
+            '/images/rocking.jpg',
+            '/images/running.jpg',
+            '/images/surfing.jpg',
+            '/css/styles.css',
+              '/js/app.js',
+            '/',
+            '/index.html']
+        );
+        })
+    );
+})
+
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            return response || fetch(event.request)
+        })
+    );
+    });
