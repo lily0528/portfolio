641387fb0a1eb1096f6aec5f1714a69b33717fde
lily0528 <43476261+lily0528@users.noreply.github.com>
2018-12-19 20:29:27 -0600

    Add files via upload
    Add js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+if(navigator.serviceWorker){
+    navigator.serviceWorker.register('../sw.js')
+    .then(() => {
+        console.log("Serve Worker Installed")
+    })
+    .catch(()=>{
+        console.log("Service Work Did not Work!")
+    })
+    }
